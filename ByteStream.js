1f81bcb2af2497cfb7b9b1d36406ddc95c4e74c4
SunboX <fiedler.andre@gmail.com>
2009-06-27 11:47:27 +0200

    more work on ByteStream

--- ByteStream.js
+++ ByteStream.js
@@ -42,12 +42,92 @@ ByteStream = new Class(
 		return this.data;
 	},
 	
+	getLength: function()
+	{
+		return this.options.length;
+	},
+	
 	getByteAt: function(offset)
 	{
 		if(this.use_vb_script)
 			return VB_Binary_getByteAt(this.data, this.offset + offset);
 			
 		return this.data.charCodeAt(this.offset + offset) & 0xFF;
+	},
+
+	getSByteAt: function(offset)
+	{
+		var b = this.getByteAt(offset);
+		if(b > 127)
+			return b - 256;
+		else
+			return b;
+	},
+
+	getShortAt: function(offset, bigEndian)
+	{
+		var s = bigEndian ? 
+			(this.getByteAt(offset) << 8) + this.getByteAt(offset + 1)
+			: (this.getByteAt(offset + 1) << 8) + this.getByteAt(offset)
+		if(s < 0) s += 65536;
+		return s;
+	},
+	
+	getSShortAt: function(offset, bigEndian)
+	{
+		var us = this.getShortAt(offset, bigEndian);
+		if(us > 32767)
+			return us - 65536;
+		else
+			return us;
+	},
+	
+	getLongAt: function(offset, bigEndian)
+	{
+		var b1 = this.getByteAt(offset),
+			b2 = this.getByteAt(offset + 1),
+			b3 = this.getByteAt(offset + 2),
+			b4 = this.getByteAt(offset + 3);
+
+		var l = bigEndian ? 
+			(((((b1 << 8) + b2) << 8) + b3) << 8) + b4
+			: (((((b4 << 8) + b3) << 8) + b2) << 8) + b1;
+		if(l < 0) l += 4294967296;
+		return l;
+	},
+	
+	getSLongAt: function(offset, bigEndian)
+	{
+		var ul = this.getLongAt(offset, bigEndian);
+		if(ul > 2147483647)
+			return ul - 4294967296;
+		else
+			return iULong;
+	},
+	
+	getStringAt: function(offset, length)
+	{
+		var str = [];
+		for(var i = offset, j = 0; i < offset + length; i++, j++)
+		{
+			str[j] = String.fromCharCode(this.getByteAt(i));
+		}
+		return str.join('');
+	},
+
+	getCharAt: function(offset)
+	{
+		return String.fromCharCode(this.getByteAt(offset));
+	},
+	
+	toBase64: function()
+	{
+		return window.btoa(this.data);
+	},
+	
+	fromBase64: function(strBase64)
+	{
+		this.data = window.atob(strBase64);
 	},
 	
 	execVBScript: function()
